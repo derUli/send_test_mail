337dee3a5296e1d4dc18a9b5f934ff46d04b15e8
Ulrich Schmidt <admin@deruli.de>
2017-06-26 17:47:33 +0200

    testmailsender vervollständigt

--- src/content/modules/send_test_mail/objects/TestMailSender.php
+++ src/content/modules/send_test_mail/objects/TestMailSender.php
@@ -3,12 +3,12 @@ class TestMailSender {
 	private $to = null;
 	private $headers = null;
 	public function __construct($to = null, $headers = null) {
-		if (is_string ( $to )) {
+		if (StringHelper::isNotNullOrWhitespace ( $to )) {
 			$this->to = $to;
 		} else {
 			$this->to = Settings::get ( "email" );
 		}
-		if (is_string ( $headers )) {
+		if (StringHelper::isNotNullOrWhitespace ( $headers )) {
 			$this->headers = $headers;
 		} else {
 			$this->headers = "From: " . Settings::get ( "email" ) . "\r\n" . "Content-Type: text/plain; charset=UTF-8";
@@ -21,12 +21,12 @@ class TestMailSender {
 		return $this->headers;
 	}
 	public function setTo($val) {
-		$this->to = is_string ( $val ) ? $val : null;
+		$this->to = StringHelper::isNotNullOrWhitespace ( $val ) ? $val : null;
 	}
 	public function setHeaders($val) {
-		$this->headers = is_string ( $val ) ? $val : null;
+		$this->headers = StringHelper::isNotNullOrWhitespace ( $val ) ? $val : null;
 	}
 	public function send() {
-		throw new NotImplementedException ( "send test mail not implemented yet" );
+		Mailer::send ( $this->getTo (), get_translation ( "test_mail_subject" ), get_translation ( "test_mail_body" ), $this->getHeader () );
 	}
 }
